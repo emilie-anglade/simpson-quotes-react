30c18f804c56be878d16c74541477da803d5ebaa
Emilie Anglade <anglade.em@gmail.com>
2020-10-13 15:12:37 +0200

    commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <nav className="Navbar">
+        <a href="https://fr.wikipedia.org/wiki/Les_Simpson">Simpson Quotes</a>
+        </nav>
+    );
+}
+
+export default Navbar;
