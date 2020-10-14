01a3aaa658bbce3e89e93bac156f851d1352b6dc
Emilie Anglade <anglade.em@gmail.com>
2020-10-14 10:32:53 +0200

    quotelist avec map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import QuoteCard from './components/QuoteCard';
 
+import QuoteList from './components/QuoteList';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <QuoteCard
+      <QuoteList />
+      
+      {/* <QuoteCard
         image="https://pm1.narvii.com/7070/aeaeadea89d89327a8e5f4f83d10272dc337425fr1-1200-1200v2_128.jpg"
         character="Nelson Muntz"
         quote="Shoplifting is a victimless crime, like punching someone in the dark."
@@ -21,7 +23,7 @@ function App() {
       image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512083" 
       character="Lisa Simpson"
       quote="Hello world!"
-      />
+      /> */}
     </div>
   );
 }
